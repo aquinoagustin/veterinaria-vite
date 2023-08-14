0bd72ef44bf889abfc3c1c3fc63e230d19b62d9c
Agus2020 <aagustin067@gmail.com>
2023-08-14 20:51:16 -0300

    Creando los primeros componentes, y utilizando tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+            <h1 className="font-black text-5xl text-center mx-auto md:w-2/3">
+                Seguimiento Pacientes {""}
+                <span className="text-indigo-600">Veterinaria</span>
+            </h1>
+    )
+}
