0d960dac9768293aeaed21b83d438a2f4597bc06
Agus2020 <aagustin067@gmail.com>
2023-08-15 23:02:37 -0300

    Terminando maquetacion

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,5 +1,17 @@
+import Paciente from './Paciente';
 export default function ListadoPacientes(){
     return(
-        <h2>Listado Pacientes</h2>
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-scroll">
+            <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">
+                Administra tus {''}
+                <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+            </p>
+            <Paciente/>
+            <Paciente/>
+            <Paciente/>
+            <Paciente/>
+            <Paciente/>
+        </div>
     )
 }
