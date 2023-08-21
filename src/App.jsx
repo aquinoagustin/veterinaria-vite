9ff1b91baea8806c3c9015ff5da6162a3f481fc8
Agus2020 <aagustin067@gmail.com>
2023-08-20 23:38:46 -0300

    Agregando localstorage, funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState,useEffect } from "react"
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
-
 function App() {
-  const [pacientes,setPacientes] = useState([]);
+  const [pacientes,setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes'))??[]);
   const [paciente,setPaciente] = useState({});
   const eliminarPaciente = id=>{
     const pacientesActualizados = pacientes.filter(paciente=>paciente.id !== id);
     setPacientes(pacientesActualizados)
   }
+  useEffect(()=>{
+    localStorage.setItem('pacientes',JSON.stringify(pacientes))
+  },[pacientes])
   return (
       <div className="container mx-auto m-20">
         <Header/>
